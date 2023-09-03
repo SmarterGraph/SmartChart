8996fc8e072d796b6c753ab0ad7c2f6d37e60452
rui <rui@solaranalytics.com>
2023-09-03 21:56:01 +1000

    added basic project files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='smart-chart',
+    version='0.0.1',
+    packages=find_packages(),
+    description="Smart Chart Made by Text Description",
+    long_description=open('README.md').read(),
+    author='Rui Tang, Yikai Yang',
+    author_email='tomtrac@hotmail.com',
+    url='https://github.com/SmarterGraph/SmartChart'
+)
