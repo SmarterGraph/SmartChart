3a43d60b6c11efc856d2c78e1ddcc16833d89d04
Yikai <yangyikai28@gmail.com>
2023-09-05 21:36:13 +1000

    add prompt base class and the prompt for matplotlib

---
+++ smart-chart/prompts/base.py
@@ -0,0 +1,28 @@
+class BasePrompt:
+    """
+    Base class for creating and formatting prompts.
+    """
+
+    prompt_context = None
+    key_words = {}
+
+    def __init__(self, **kwargs) -> None:
+        """
+        Initialize prompt.
+
+        Args:
+            **kwargs: Keyword arguments.
+        """
+        if kwargs:
+            self.key_words = kwargs
+
+    def __str__(self) -> str:
+        """
+        String representation of prompt.
+
+        Returns:
+            str: String representation of prompt.
+        """
+        if self.prompt_context is None:
+            print("Prompt Context is not set.")
+        return self.context.format(**self.key_words)
